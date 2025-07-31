f1229988fe8ff18e610ae408fce92122970a0a4b
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-07-31 18:16:27 +0200

    Build assimp

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,16 +6,17 @@ $ErrorActionPreference = "Stop"
 Write-Host "Building with Emscripten for WebAssembly..." -ForegroundColor Green
 Write-Host "Build Type: " -NoNewline -ForegroundColor Green
 Write-Host $BuildType -ForegroundColor Cyan
-emcmake cmake -B build-wasm -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BuildType .
+
+emcmake cmake -B build-wasm -G "Ninja" -DCMAKE_BUILD_TYPE="$BuildType" .
 if (!$?) {
     Write-Host "CMake configuration failed. Exiting."
     exit 1
 }
-Write-Host "Makefile generated, proceeding to build..." -ForegroundColor Green
-emmake make -C build-wasm
+
+Write-Host "Ninja files generated, proceeding to build..." -ForegroundColor Green
+emmake ninja -C build-wasm
 if (!$?) {
     Write-Host "Build failed. Exiting."
     exit 1
 }
 Write-Host "Build completed successfully!" -ForegroundColor Green
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <assimp/version.h>
+
+
+std::string getAssimpVersion() {
+    return std::to_string(aiGetVersionMajor()) + "." +
+           std::to_string(aiGetVersionMinor()) + "." +
+           std::to_string(aiGetVersionPatch());
+}
 
 int main() {
-  printf("hello, world!\n");
-  return 0;
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "Assimp Version: " << getAssimpVersion() << std::endl;
+    return 0;
 }
